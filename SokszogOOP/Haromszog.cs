32906c1d8aadfa7176da6d4b3424bdbe0f9e6a92
Tibi Halasi <czalbert.tibor04@gmail.com>
2023-09-28 09:16:01 +0200

    Haromszog és Teglalap osztály befejezése

--- SokszogOOP/Haromszog.cs
+++ SokszogOOP/Haromszog.cs
@@ -8,6 +8,7 @@ namespace SokszogOOP
 {
     internal class Haromszog : Sokszog
     {
+        private static Random random = new Random();
         private double b;
         private double c;
 
@@ -17,17 +18,34 @@ namespace SokszogOOP
             this.c = c;
         }
 
+        public Haromszog() : base(VeletlenOldalhossz())
+        {
+            this.b = VeletlenOldalhossz();
+            this.c = VeletlenOldalhossz();
+        }
+
+        private static int VeletlenOldalhossz()
+        {
+            return random.Next(5, 15);
+        }
+
         public double B { get => b; set => b = value; }
         public double C { get => c; set => c = value; }
 
-        public double getKerulet()
+        public double GetKerulet()
         {
             return this.A + this.b + this.c;
         }
-        
-        public double getTerulet()
+
+        public double GetTerulet()
         {
-            return Math.Sqrt((getKerulet() / 2) * (getKerulet() - this.A) * (getKerulet() - this.b) * (getKerulet() - this.c));
+            double s = GetKerulet() / 2;
+            return Math.Sqrt(s * (s - this.A) * (s - this.b) * (s - this.c));
+        }
+
+        public override string ToString()
+        {
+            return $"a:{this.A}  b:{this.b}  c:{this.c}  Kerület:{GetKerulet()}  Terület:{GetTerulet()}";
         }
     }
 }
--- SokszogOOP/Teglalap.cs
+++ SokszogOOP/Teglalap.cs
@@ -17,14 +17,19 @@ namespace SokszogOOP
 
         public double B { get => b; set => b = value; }
 
-        public double getKerulet()
+        public double GetKerulet()
         {
             return 2 * (this.A + this.b);
         }
 
-        public double getTerulet()
+        public double GetTerulet()
         {
             return this.A * this.b;
+        }
+
+        public override string ToString()
+        {
+            return $"a:{this.A}  b:{this.b} Kerület:{GetKerulet()}  Terület:{GetTerulet()}";
         }
     }
 }
